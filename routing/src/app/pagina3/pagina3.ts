bf6785549dc74b62f83e762fd5d9c7c72839a757
LoryMorty <168710437+LoryMorty@users.noreply.github.com>
2025-10-27 12:51:54 +0000

    fatto l'esercizio 1

--- routing/src/app/pagina3/pagina3.ts
+++ routing/src/app/pagina3/pagina3.ts
@@ -1,15 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Studenti } from '../studenti/studenti';
 
 @Component({
   selector: 'app-pagina3',
-  imports: [CommonModule],
+  imports: [CommonModule, Studenti],
   templateUrl: './pagina3.html',
   styleUrl: './pagina3.css',
 })
 export class Pagina3 {
   studenti = [
-{ id: 1, nome: 'Mario', classe: '5D', mediaVoti: 4.1 },
+{ id: 1, nome: 'Mario', classe: '5E', mediaVoti: 4.1 },
 { id: 2, nome: 'Luca', classe: '5D', mediaVoti: 7.3 },
 { id: 3, nome: 'Luigi', classe: '5D', mediaVoti: 3.9 },
 { id: 4, nome: 'Paolo', classe: '5D', mediaVoti: 7.0 },
